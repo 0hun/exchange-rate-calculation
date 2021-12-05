1894dc5568b76af703e4643a37d173e64e33fa40
younghun <whdudgns2654@gmail.com>
2021-12-06 00:53:35 +0900

    test (when adding missing tests) : 환율 조회 테스트 코드 수정
    test (when adding missing tests) : 환율 조회 테스트 코드 수정
    - 결과 json parsing하여 체크하도록 수정

--- src/test/java/com/example/exchangeratecalculation/infra/CurrencyClientTest.java
+++ src/test/java/com/example/exchangeratecalculation/infra/CurrencyClientTest.java
@@ -2,20 +2,35 @@ package com.example.exchangeratecalculation.infra;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = CurrencyLayerClient.class, properties = "classpath:application.properties")
 public class CurrencyClientTest {
 
+    @Autowired
+    CurrencyClient currencyClient;
+
+    @DisplayName("국가별 환율 가져오기")
     @Test
-    void send() {
+    void getCurrentCurrencies() throws Exception{
         // given
-        CurrencyClient currencyClient = new CurrencyLayerClient();
+        JSONParser parser = new JSONParser();
 
         // when
         String result = currencyClient.currentCurrencies();
+        JSONObject jsonObj = (JSONObject) parser.parse(result);
+        boolean isSuccess = (boolean) jsonObj.get("success");
 
         // then
-        assertThat(result).isNotNull();
+        assertThat(isSuccess).isTrue();
     }
 
 }
